9d0f530ab5ec6aa5640788789f3464aeef4d8c02
edwardengeek-cloud <edwardengeek@gmail.com>
2026-07-24 02:34:44 +0800

    Update lib.rs

--- en.weloma/src/lib.rs
+++ en.weloma/src/lib.rs
@@ -9,15 +9,18 @@ const BASE_URL: &str = "https://weloma.art";
 #[get_manga_list]
 fn get_manga_list(_filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
     let url = format!("{}/manga-list.html?sort=last_update&sort_type=DESC&page={}", BASE_URL, page);
-    let html = Request::new(url.as_str(), HttpMethod::Get).html()?;
-    Ok(MangaPageResult { manga: Vec::new(), has_more: true }) // Temporary
+    let _html = Request::new(url.as_str(), HttpMethod::Get).html()?;
+    Ok(MangaPageResult {
+        entries: Vec::new(),
+        has_next_page: true,
+    })
 }
 
 #[get_manga_details]
 fn get_manga_details(manga_id: String) -> Result<Manga> {
     Ok(Manga {
-        id: manga_id,
-        title: "Test".to_string(),
+        key: manga_id,
+        title: "Test Manga".to_string(),
         ..Default::default()
     })
 }
@@ -34,5 +37,8 @@ fn get_page_list(_manga_id: String, _chapter_id: String) -> Result<Vec<Page>> {
 
 #[handle_url]
 pub fn handle_url(url: String) -> Result<DeepLink> {
-    Ok(DeepLink { manga: None, chapter: None })
+    Ok(DeepLink {
+        manga: None,
+        chapter: None,
+    })
 }
